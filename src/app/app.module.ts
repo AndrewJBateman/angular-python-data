8d5e77f1f64a3ef880f31a711afa1a74be77fb04
AndrewJBateman <gomezbateman@yahoo.com>
2022-04-04 20:36:25 +0200

    feat: add frontend dependencies & chart +data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,37 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './modules/home/home.component';
+
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore()),
-    AngularFireDatabaseModule
+
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,15 +1,179 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AngularFirestore
+} from "@angular/fire/compat/firestore";
+import { debounceTime } from 'rxjs/operators';
+
+import { Chart, registerables } from 'chart.js';
+import firebase from 'firebase/compat/app';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  public canvasCO2: any; // HTML canvas
+  public ctxCO2: any; // context for HTML canvas
+  public chartCO2: any; // chart object for Chart.js
 
-  constructor() { }
+  public CO2SensorReading: any; // form input of new sensor value
+  public condensateSensorReading: any;
 
-  ngOnInit(): void {
+  public historicalCO2: any[]; // stored data from Firebase (or hardcoded for testing)
+
+  public currentSensorReadings: any; // current sensor readings for a given collection
+
+  constructor(private firestore: AngularFirestore) {
+    this.historicalCO2 = [];
   }
 
+  ngOnInit(): void {
+    this.initCO2Chart();
+
+    let docNameCurrent = 'current'; // only storing the current sensor readings
+    let docNameHistoric = this.buildHistoricDocName(); // storing an array of all sensor readings for the time bucket
+
+    this.currentSensorReadings = this.firestore
+      .collection('LNG_Process')
+      .doc(docNameCurrent)
+      .valueChanges(); // attach to the observable so HTML updated
+
+    this.firestore
+      .collection('LNG_Process')
+      .doc(docNameCurrent)
+      .valueChanges()
+      .pipe(debounceTime(200))
+      .subscribe((data: any) => {
+        if (data && data.hasOwnProperty('level_CO2'))
+          this.addDataToChart(this.chartCO2, '', data['level_CO2']); // when the 'current' doc changes, place the new CO2 value in the chart
+      });
+
+    this.firestore
+      .collection('LNG_Process')
+      .doc(docNameHistoric)
+      .ref.get()
+      .then((doc) => {
+        // get the current hour's historical readings just once (without an observable)
+        if (doc.exists) {
+          let data: any = doc.data();
+          let measurements = data['historicalMeasurements'];
+
+          console.log('measurements', measurements);
+
+          for (let i = 0; i < measurements.length; i++) {
+            let measurement = measurements[i];
+            let CO2 = measurement['level_CO2'];
+            this.addDataToChart(this.chartCO2, '', CO2);
+          }
+        }
+      })
+      .catch((error) => {
+        console.log('Error getting historical doc:', error);
+      });
+
+    this.randomizeSensorReadings(); // set our form inputs to random values
+  }
+
+  initCO2Chart() {
+    this.canvasCO2 = document.getElementById('chartCO2');
+    this.ctxCO2 = this.canvasCO2.getContext('2d');
+    this.chartCO2 = new Chart(this.ctxCO2, {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: 'CO2',
+            data: [],
+            fill: false,
+            borderColor: 'rgb(44, 11, 179)',
+            borderWidth: 1,
+            pointStyle: 'rectRot',
+            pointRadius: 2,
+            pointBorderColor: 'rgb(0, 0, 0)',
+            tension: 0.1,
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: 'Chart of LNG Process Variables',
+          },
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+
+  randomizeSensorReadings() {
+    this.CO2SensorReading = this.generateRandomInt(0, 100);
+    this.condensateSensorReading = this.generateRandomInt(60, 80);
+  }
+
+  generateRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  insertData() {
+    let docNameCurrent = 'current'; // only storing the current sensor readings
+    let docNameHistoric = this.buildHistoricDocName(); // storing an array of all sensor readings for the time bucket
+
+    this.firestore.collection('LNG_Process').doc(docNameCurrent).set({
+      CO2: this.CO2SensorReading,
+      temperature: this.condensateSensorReading,
+      // timestamp: firebase.firestore.Timestamp.now(),
+      timestamp: firebase.firestore.Timestamp.now(),
+    });
+
+    this.firestore
+      .collection('LNG_Process')
+      .doc(docNameHistoric)
+      .set(
+        {
+          historicalMeasurements: firebase.firestore.FieldValue
+          .arrayUnion({
+            CO2: this.CO2SensorReading,
+            condensate: this.condensateSensorReading,
+            timestamp: firebase.firestore.Timestamp.now(),
+          }),
+        },
+        { merge: true }
+      ); // provides an update and creates the doc if it doesn't exist
+
+    this.randomizeSensorReadings(); // generate new random values for next time
+  }
+
+  buildHistoricDocName() {
+    let now = new Date();
+
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1 + ''; // javascript months are zero-based, so add 1
+    let day = now.getDate() + '';
+    let hour = now.getHours() + '';
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    if (hour.length < 2) hour = '0' + hour;
+    hour = 'h' + hour;
+
+    return [year, month, day, hour].join('_'); // ex: '2021_05_13_h09'
+  }
+
+  addDataToChart(chart: any, label: string, data: number) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset: any) => {
+      dataset.data.push(data);
+    });
+    chart.update();
+  }
 }
